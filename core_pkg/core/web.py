80c2a3935efbd7b2a852900161de6203b7d2ed7b
luminar <kovykov@webanet.cz>
2019-01-09 20:39:14 +0100

    Improve colors Remove __name__ clause

--- core_pkg/core/web.py
+++ core_pkg/core/web.py
@@ -2,16 +2,36 @@
 chemicals and pollution measurements data."""
 
 import json
-import random
-import string
 from typing import Callable, Generator, List, Union
 
 from core.chemicals import get_chemical
 from core.measurements import get_measurements, group_by_region, group_by_year
 
 
-def color():
-    return "#" + "".join(random.choices(string.hexdigits, k=6))
+def color_generator():
+    """Return a generator that yields distinctive colors."""
+
+    return (
+        c
+        for c in [
+            "#e6194B",
+            "#3cb44b",
+            "#ffe119",
+            "#4363d8",
+            "#f58231",
+            "#42d4f4",
+            "#f032e6",
+            "#fabebe",
+            "#469990",
+            "#e6beff",
+            "#9A6324",
+            "#fffac8",
+            "#800000",
+            "#aaffc3",
+            "#000075",
+            "#a9a9a9",
+        ]
+    )
 
 
 def get_datasets(
@@ -30,11 +50,13 @@ def get_datasets(
 
     ms = get_measurements(chemical)
 
+    colors = color_generator()
+
     for region_group in group_by_region(ms):
         region_name = region_group[0].region
 
         if not regions or region_name in regions:
-            c = color()
+            c = next(colors)
             dataset = {"label": region_name, "backgroundColor": c, "borderColor": c}
             data = {}
             for year_group in group_by_year(region_group):
@@ -54,71 +76,3 @@ def get_datasets(
 
 def avg_value(group):
     return sum(m.value for m in group) / len(group)
-
-
-if __name__ == "__main__":
-    years = list(range(2004, 2013))
-
-    chemical_iri = "oxid-uhličitý-co2-"
-    chemical = get_chemical(chemical_iri)
-    if chemical:
-        data = {
-            "labels": years,
-            "datasets": list(get_datasets(chemical, years=years, fun=avg_value)),
-        }
-
-        top = (
-            """
-        <!DOCTYPE html>
-
-        <html>
-          <head>
-            <title>%s</title>
-            <script type="text/javascript" charset="utf-8" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
-          </head>
-
-          <body>
-
-            <div>
-              <canvas id="myChart"></canvas>
-            </div>
-
-            <script>
-            var ctx = document.getElementById("myChart").getContext('2d');
-            var myChart = new Chart(ctx, {
-                type: 'bar',
-                data:
-        """
-            % chemical_iri
-        )
-
-        bottom = """
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero:true
-                            }
-                        }]
-                    }
-                }
-            });
-            </script>
-          </body>
-        </html>
-        """
-
-        with open("index.html", "w") as f:
-            f.write(top)
-            json.dump(data, f, ensure_ascii=False)
-            f.write(",")
-            f.write(bottom)
-
-        from http.server import HTTPServer, SimpleHTTPRequestHandler
-
-        def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
-            server_address = ("", 8000)
-            httpd = server_class(server_address, handler_class)
-            httpd.serve_forever()
-
-        run()
