36062475853bb9b2e0e57a67fcd137f7da4967ca
luminar <kovykov@webanet.cz>
2019-01-11 22:51:51 +0100

    Add parameter form

--- app/app.py
+++ app/app.py
@@ -1,13 +1,11 @@
 import json
 import logging
-import sys
 
 from flask import Flask, render_template, request
+from wtforms import Form, fields
 
 from core import chemicals, web
 from core.measurements import AGGREGATORS
-
-print(sys.argv[0])
 
 logger = logging.getLogger(__name__)
 
@@ -15,14 +13,30 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
 
+class ParamForm(Form):
+    all_chems = [
+        (c.iri, c.name) for c in sorted(chemicals.get_all_chemicals().values())
+    ]
+    functions = [(k, v.get("label")) for k, v in AGGREGATORS.items()]
+
+    chemical = fields.SelectField("Chemikálie", choices=all_chems)
+    fun = fields.SelectField("Agregační funkce", choices=functions)
+
+
 @app.route("/")
 def index():
-    chem_iri = request.args.get("chemical", "naftalen")
-    fun = request.args.get("fun", "len")
+    chem_iri = request.args.get("chemical", "chemical=oxid-uhličitý-co2-")
+    fun = request.args.get("fun", "mean")
+    unit = AGGREGATORS.get(fun).get("unit", "t")
+    label = AGGREGATORS.get(fun).get("label", "Průměrné emise")
+
+    form = ParamForm(fun=fun, chemical=chem_iri)
 
     chemical = chemicals.get_chemical(chem_iri)
 
-    return render_template("index.html", fun=fun, chemical=chemical)
+    return render_template(
+        "index.html", fun=fun, chemical=chemical, form=form, unit=unit, label=label
+    )
 
 
 @app.route("/api/get/datasets/<iri>")
@@ -30,7 +44,7 @@ def datasets(iri):
     years = list(range(2004, 2013))
 
     try:
-        fun = AGGREGATORS.get(request.args.get("fun", "len"), len)
+        fun = AGGREGATORS.get(request.args.get("fun", "len")).get("fun", len)
 
         datasets = list(web.get_datasets(iri, years=years, fun=fun))
     except:
